87c63195da525aa7b02412a8ef5bab88e2556826
whitlocktech <whitlocktech@gmail.com>
2023-05-14 00:49:56 -0500

    init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Home of my future Portfolio
+          <br /> For now visit my github
+          <br />
+          <a href="https://github.com/whitlocktech">whitlocktech</a> 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
